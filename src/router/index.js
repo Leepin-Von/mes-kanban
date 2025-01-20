d8fbbed5d9d27cc0107353d1fe158ab8b30dd478
Leepin-Von <lipingvon1111@outlook.com>
2025-01-20 11:51:22 +0800

    优化路由守卫逻辑，确保未授权用户重定向到登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -26,14 +26,14 @@ const router = new VueRouter({
 // 路由守卫
 router.beforeEach((to, from, next) => {
   if (to.path === "/") {
-    return next("/signIn");
+    return next({ path: "/signIn" });
   }
   const token = localStorage.getItem("Authorization");
   const publicPages = ["/signIn"];
   const authRequired = !publicPages.includes(to.path);
 
   if (authRequired && (!token || isTokenExpired(token))) {
-    return next();
+    return next({ path: "/signIn" });
   } else if (to.path === "/signIn" && token) {
     if (to.query.redirect) {
       return next(to.query.redirect); // 重定向到原始页面
