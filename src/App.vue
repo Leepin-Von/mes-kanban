0758f69ea9e92add2fb6e65a9ed93d05d43bdfcd
Leepin-Von <lipingvon1111@outlook.com>
2025-06-24 13:38:54 +0800

    refactor(App.vue): remove unused version check and footer display logic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div id="app" :data-version="ver">
+  <div id="app">
     <router-view />
-    <div v-show="isFooterShow" class="footer">当前版本：{{ curVer }}</div>
   </div>
 </template>
 
@@ -11,41 +10,23 @@ import PackageJson from "../package.json";
 export default {
   data() {
     return {
-      curVer: "",
-      ver: PackageJson.version,
-      updateMessage: `
-      新增：签核中心Beta版
-      修复：顶部导航栏无法显示的问题
-      `,
+      version: PackageJson.version,
     };
   },
-  computed: {
-    isFooterShow() {
-      return !this.$route.path.startsWith("/home/jmreport")
-        || !this.$route.path.startsWith("/home/drag")
-        || !this.$route.path === "/home/vform_designer";
-    },
-  },
+  computed: {},
   mounted() {
-    this.checkVersion();
+    console.info(
+      "%c Environment %c " + process.env.NODE_ENV + " ",
+      "padding: 2px 6px; border-radius: 3px 0 0 3px; color: #FFF; background:#606060; font-weight: bold;",
+      "padding: 2px 6px; border-radius: 0 3px 3px 0; color: #FFF; background:#42c02e; font-weight: bold;"
+    );
+    console.info(
+      "%c it-srv-system %c v" + this.version + " ",
+      "padding: 2px 6px; border-radius: 3px 0 0 3px; color: #FFD700; background: #606060; font-weight: bold;",
+      "padding: 2px 6px; border-radius: 0 3px 3px 0; color: #FFF; background: #5C7BD9; font-weight: bold;"
+    );
   },
-  methods: {
-    checkVersion() {
-      this.curVer = localStorage.getItem("ver");
-      if (this.curVer !== this.ver) {
-        this.$alert(this.updateMessage, "新版本发布", {
-          confirmButtonText: "刷新",
-          callback: (action) => {
-            if (action === "confirm") {
-              location.reload();
-            }
-          },
-          customClass: "updateMsg",
-        });
-        localStorage.setItem("ver", this.ver);
-      }
-    },
-  },
+  methods: {},
 };
 </script>
 
@@ -55,28 +36,9 @@ export default {
     "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-
-  .footer {
-    background-color: #fff;
-    padding-top: 3px;
-    padding-bottom: 3px;
-    font-family: "Courier New", Courier, monospace;
-    width: 100%;
-    text-align: center;
-    position: fixed;
-    bottom: 0;
-    color: #666;
-    user-select: none;
-    font-size: calc(0.5rem + 0.5vw);
-    z-index: 100;
-  }
 }
 
 body {
   margin: 0;
-}
-
-.updateMsg {
-  white-space: pre-wrap;
 }
 </style>
